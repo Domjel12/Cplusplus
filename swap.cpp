8f0cf1890d4ae86c49c31ec63c59ca50e34184e0
Dominik <156206194+Domjel12@users.noreply.github.com>
2024-05-03 11:05:28 +0200

    Add files via upload

--- swap.cpp
+++ swap.cpp
@@ -3,20 +3,37 @@
 void swap(int& i, int& j){
     int h;
 
-    h = j;
+    h = i;
     i = j;
     j = h;
 }
 
+void swap(float& i, float& j){
+    float h;
+
+    h = i;
+    i = j;
+    j = h;
+}
+
+
 int main(){
     int a = -3;
     int b = 42;
+    float x = 1.3;
+    float y = -3.4;
 
     std::cout << "Input: " << a << " , " << b << std::endl;
 
     swap(a,b);
 
     std::cout << "Output: " << a << " , " << b << std::endl;
+
+    std::cout << "Input: " << x << " , " << y << std::endl;
+
+    swap(x,y);
+
+    std::cout << "Output: " << x << " , " << y << std::endl;
 
     return 0;
 }
